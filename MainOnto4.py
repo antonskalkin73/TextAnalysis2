9587589ea6240676485712f2cbdc628507af6f16
Anton Skalkin <anton.skalkin73@gmail.com>
2020-06-24 18:13:51 +0400

    Начало экспериментов

--- MainOnto4.py
+++ MainOnto4.py
@@ -10,20 +10,19 @@ import numpy as np
 import logging
 from senticnet.senticnet import SenticNet
 
-'''
 
-'''
+listNegative = ['no', 'not']
 
 
-def reviewToWordlist(review):
-    reviewText = BeautifulSoup(review, features="html.parser").get_text()
-    reviewText = re.sub("[^a-zA-Z]", " ", reviewText)  # Оставляем только английские буквы
-    words = reviewText.lower().split()
+def reviewToWordlist(sentence):
+    sentenceText = BeautifulSoup(sentence, features="html.parser").get_text()
+    sentenceText = re.sub("[^a-zA-Z]", " ", sentenceText)  # Оставляем только английские буквы
+    words = sentenceText.lower().split()
     return (words)
 
 
-def review_to_sentences(review, tokenizer):
-    raw_sentences = tokenizer.tokenize(review.strip())
+def reviewToSentences(reviewText):
+    raw_sentences = tokenizer.tokenize(reviewText.strip())
     sentences = []
     for raw_sentence in raw_sentences:
         if len(raw_sentence) > 0:
@@ -39,105 +38,45 @@ def listWordToStr(listWord):
             string += '_'
     return string
 
+
+def getMarkSentenseList(sentenseList):
+    markSentenceList = list()
+    for sentence in sentenseList:
+        markWordList = list()
+        wordIndex = 0
+        while wordIndex < len(sentence):
+            for lenCollocation in range(min(3, len(sentence) - wordIndex), 0, -1):
+                stringWord = listWordToStr(sentence[wordIndex:wordIndex + lenCollocation])
+                collocationProperty = sn.data.get(stringWord)
+                if collocationProperty is not None:
+                    if wordIndex-1 >= 0 and sentence[wordIndex-1] in listNegative:
+                        markWordList.append([float(collocationProperty[7])*-1, sentence[wordIndex-1]+'_'+stringWord])
+                    else:
+                        markWordList.append([float(collocationProperty[7]), stringWord])
+                    break
+            wordIndex += lenCollocation
+        markSentenceList.append(markWordList)
+    return markSentenceList
+
+
+def getReviewMark(reviewText):
+    if reviewText[0].isalpha():
+        reviewText = reviewText[0].lower() + reviewText[1:]  # Перевод первого слова в нижний регистр
+    reviewText = re.sub(r'".*"', '', reviewText)  # Удаление двойных кавычек с содержимым
+    reviewText = re.sub(r'\b[A-Z]+[a-z]+\b', '', reviewText)  # Удаление слов с заглавных букв, имен собственных
+    sentenseList = reviewToSentences(reviewText)
+    markList = getMarkSentenseList(sentenseList)
+    return markList
+
+
 if __name__ == '__main__':
     start = time.time()
-    # Настройка параметров
-    # numFeatures = 300  # Количество простанств вектора слова
-    # minWordCount = 50  # Minimum word count
-    # num_workers = 16  # Number of threads to run in parallel
-    # context = 10  # Context window size
-    # downsampling = 1e-3  #  Downsample setting for frequent words
-
-    train = pd.read_csv('Data//labeledTrainData.tsv', header=0, delimiter="\t", quoting=3)
-    # unlabeled_train = pd.read_csv('Data//unlabeledTrainData.tsv', header=0, delimiter="\t", quoting=3)
-    # test = pd.read_csv('Data//testData.tsv', header=0, delimiter="\t", quoting=3)
-    # labelTest = pd.read_csv('Data//LabeledTestData.csv', header=0, delimiter=",", quoting=0)
-
+    train = pd.read_csv('Data//labeledSentensData.tsv', header=0, delimiter="\t", quoting=3)
     sn = SenticNet()
     tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-    truePAccuracy, trueNAccuracy, falseAccuracy = 0, 0, 0
-    totalMark = 0.32
-    sumError, sumTrueP, sumTrueN = 0.0, 0.0, 0.0
-    avgP, avgN = 0.0, 0.0
-    for review in train.values:
-        sentenseList = review_to_sentences(review[2], tokenizer)
-        sumMark, countWord = 0.0, 0
-        for sentense in sentenseList:
-            wordIndex = 0
-            while wordIndex < len(sentense):
-                for lenCollocation in range(min(3, len(sentense)-wordIndex), 0, -1):
-                    stringWord = listWordToStr(sentense[wordIndex:wordIndex+lenCollocation])
-                    collocationProperty = sn.data.get(stringWord)
-                    if collocationProperty is not None:
-                        countWord += 1
-                        # if abs(float(collocationProperty[1]))>0.7:
-                        sumMark += float(collocationProperty[7])
-                        break
-                wordIndex += lenCollocation
-        sumMark /= countWord
-        if sumMark > totalMark and review[1] == 1:
-            truePAccuracy += 1
-            sumTrueP += sumMark
-        elif sumMark <= totalMark and review[1] == 0:
-            trueNAccuracy += 1
-            sumTrueN += sumMark
-        else:
-            falseAccuracy += 1
-            sumError += sumMark
-        if review[1] == 1:
-            avgP += sumMark
-        else:
-            avgN += sumMark
-    sumError /= falseAccuracy
-    sumTrueP /= truePAccuracy
-    sumTrueN /= trueNAccuracy
-    avgN /= 12500
-    avgP /= 12500
-    print()
 
-    # Verify the number of reviews that were read (100,000 in total)
-    # print("Read %d labeled train reviews, %d labeled test reviews, "
-    #       "and %d unlabeled reviews\n" % (train["review"].size,
-    #                                       test["review"].size, unlabeled_train["review"].size))
+    resultReviews = [ [review[0], getReviewMark(review[1]) ] for review in train.values]
 
 
-
-    # modelName = "big300features_40minwords_10context"
-    #
-    # # Создание модели Word2Vec
-    # createModel(modelName)
-    #
-    # model = Word2Vec.load('Model//' + modelName)
-    # word_vectors = model.wv.vectors
-    # num_clusters = int(word_vectors.shape[0] / 10) # Рекомендуется 5 слов на кластер
-    # print("Running K means, where K = ", num_clusters)
-    # kmeans_clustering = KMeans(n_clusters=num_clusters)
-    # idx = kmeans_clustering.fit_predict(word_vectors)
-    # word_centroid_map = dict(zip(model.wv.index2word, idx))
-    #
-    # train_centroids = createCentroids(train)
-    # test_centroids = createCentroids(test)
-    # labelTest_centroids = createCentroids(labelTest)
-    #
-    # forest = RandomForestClassifier(n_estimators=100)
-    # print("Fitting a random forest to labeled training data...")
-    # forest = forest.fit(train_centroids, train["sentiment"])
-    #
-    # result = forest.predict(test_centroids)
-    # output = pd.DataFrame(data={"id": test["id"], "sentiment": result})
-    # output.to_csv("Data//Result.csv", index=False, quoting=3)
-    #
-    # deviation = forest.predict(labelTest_centroids) # deviation - отклонение(ошибка)
-    # deviationDF = pd.DataFrame(data={"id": labelTest["id"], "sentiment" : labelTest["sentiment"],
-    #                                  "deviationSentiment" : deviation})
-    # deviationDF.to_csv("Data//ResultDeviation.csv", index=False, quoting=3)
-    #
-    # deviationCount = 0.0
-    # for index, row in deviationDF.iterrows():
-    #     if row["sentiment"] == row["deviationSentiment"]:
-    #         deviationCount += 1
-    # print("Количество слов: ", word_vectors.shape[0] )
-    # print("Количество простанств вектора слова: ", numFeatures)
-    # print("Минимальное кол-во слова: ", minWordCount)
-    # print("Точность: ", (deviationCount / len(deviationDF)))
     print("Done: ", time.time() - start, "seconds.")
+    print()
